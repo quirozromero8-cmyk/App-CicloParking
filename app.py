91faa5e524293a95648b47f0d0a7d7acf2d54caa
quirozromero8-cmyk <quirozromero8@gmail.com>
2026-04-22 20:40:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import geopandas as gpd
 import streamlit as st
 from streamlit_js_eval import get_geolocation
 from streamlit_folium import st_folium
+import requests
 loc = get_geolocation()
 
 df = pd.read_csv("Cicloparqueadero.csv")
@@ -30,21 +31,31 @@ gdf = gpd.GeoDataFrame(
     crs="EPSG:4326"
 )
 
+
+#Ubication user
 if loc:
     user_lat = loc['coords']['latitude']
     user_lon = loc['coords']['longitude']
 
-mapa = folium.Map(location=[user_lat, user_lon], tiles="CartoDB Positron", zoom_start=18)
 
-gdf["Distancia_Km"] = ((gdf["Y"] - user_lat).abs() + (gdf["X"] - user_lon).abs())*111
+mapa = folium.Map(location=[user_lat, user_lon], tiles="OpenStreetMap", zoom_start=15)
+
+#Distance in m user - parking
+gdf["Distancia_m"] = (((gdf["Y"] - user_lat).abs() + (gdf["X"] - user_lon).abs()) * 111000).astype(int)
+
+# Recomendations based on category and distance
+oro   = gdf[gdf["Categoria"] == "Oro"].sort_values("Distancia_m")
+otros = gdf[gdf["Categoria"] != "Oro"].sort_values("Distancia_m")
+recomendados = pd.concat([oro.head(1), otros.head(2)]).reset_index(drop=True) #Olny the 3 best parkings
+
 
 folium.Marker(
     location=[user_lat, user_lon],
-    tooltip="Uicacion actual",
+    tooltip="Ubicacion actual",
     icon=folium.Icon(color='red', icon='user', prefix='fa')
 ).add_to(mapa)
 
-for _, row in gdf.iterrows():
+for _, row in recomendados.iterrows():
     color = "lightblue" if row["Categoria"] == "Oro" else "lightgray"
     folium.Marker(
         location=[row["Y"], row["X"]],
@@ -53,9 +64,9 @@ for _, row in gdf.iterrows():
     ).add_to(mapa)
 
 # App
-tabla = gdf[["Nombre", "Distancia_Km"]].sort_values("Distancia_Km").head(3)
-tabla = tabla.rename(columns={"Distancia_Km": "Distancia (Km)"})
-tabla = tabla.reset_index(drop=True)
+tabla = recomendados[["Nombre", "Distancia_m", "Categoria"]]
+tabla = tabla.rename(columns={"Distancia_m": "Distancia (m)"})
+
 st.write(tabla)
 st.title("CicloParking")
 st.write("Encuentra el cicloparqueadero más cercano a ti.")
